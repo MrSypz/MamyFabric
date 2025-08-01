e63048e256feb5ccc4ba495ba4a7fef444e5cb17
chain <chainot@hotmail.co.th>
2025-08-01 19:47:50 +0700

    Remove Outter Class Merge into one

--- src/main/java/com/sypztep/mamy/client/util/TextParticleProvider.java
+++ src/main/java/com/sypztep/mamy/client/util/TextParticleProvider.java
@@ -1,7 +1,13 @@
 package com.sypztep.mamy.client.util;
 
+import com.sypztep.mamy.client.particle.TextParticle;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 
 import java.awt.*;
 import java.util.function.Supplier;
@@ -104,6 +110,21 @@ public final class TextParticleProvider {
     private static void spawnTextParticle(Entity target, Text text, Color color, float maxSize, float yPos) {
         if (target.getWorld().isClient()) {
             TextParticleClient.spawnParticle(target, text.getString(), color, maxSize, yPos);
+        }
+    }
+    @Environment(EnvType.CLIENT)
+    public static final class TextParticleClient {
+        public static void spawnParticle(Entity target, String text, Color color, float maxSize, float yPos) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            ClientWorld world = client.world;
+            if (world == null || !world.isClient()) return;
+
+            Vec3d particlePos = target.getPos().add(0.0, target.getHeight() + 0.95 + yPos, 0.0);
+            TextParticle particle = new TextParticle(world, particlePos.x, particlePos.y, particlePos.z);
+            particle.setText(text);
+            particle.setColor(color.getRed(), color.getGreen(), color.getBlue());
+            particle.setMaxSize(maxSize);
+            client.particleManager.addParticle(particle);
         }
     }
 }
