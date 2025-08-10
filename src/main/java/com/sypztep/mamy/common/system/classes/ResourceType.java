06c3f3fe330c53ab07a736e348d7e89b6200c09a
chain <chainot@hotmail.co.th>
2025-08-10 12:30:20 +0700

    Remvoe unused field

--- src/main/java/com/sypztep/mamy/common/system/classes/ResourceType.java
+++ src/main/java/com/sypztep/mamy/common/system/classes/ResourceType.java
@@ -1,27 +1,17 @@
 package com.sypztep.mamy.common.system.classes;
 
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-
 public enum ResourceType {
-    MANA(0x3366FF, "Mana", Formatting.BLUE),
-    RAGE(0xFF3333, "Rage", Formatting.YELLOW);
+    MANA(0x3366FF, "Mana"),
+    RAGE(0xFF3333, "Rage");
 
     private final int color;
     private final String displayName;
-    private final Formatting textColor;
 
-    ResourceType(int color, String displayName, Formatting textColor) {
+    ResourceType(int color, String displayName) {
         this.color = color;
         this.displayName = displayName;
-        this.textColor = textColor;
     }
 
     public int getColor() { return color; }
     public String getDisplayName() { return displayName; }
-    public Formatting getTextColor() { return textColor; }
-
-    public Text getFormattedName() {
-        return Text.literal(displayName).formatted(textColor);
-    }
 }
