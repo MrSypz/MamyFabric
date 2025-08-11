ff66b47d5e84274c602e4cf290a64a80955e99a5
chain <chainot@hotmail.co.th>
2025-08-12 01:58:10 +0700

    remove dupe tag

--- src/main/java/com/sypztep/mamy/client/provider/ModDamageTypeTagProvider.java
+++ src/main/java/com/sypztep/mamy/client/provider/ModDamageTypeTagProvider.java
@@ -20,46 +20,20 @@ public class ModDamageTypeTagProvider extends FabricTagProvider<DamageType> {
 	@Override
 	protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
 		// ==========================================
-		// PHYSICAL DAMAGE - Non-magical physical forces
-		// ==========================================
-		getOrCreateTagBuilder(ModTags.DamageTags.PHYSICAL_DAMAGE)
-				.add(DamageTypes.MOB_ATTACK)           // Melee attacks from mobs
-				.add(DamageTypes.PLAYER_ATTACK)        // Melee attacks from players
-				.add(DamageTypes.FALLING_BLOCK)        // Falling blocks (sand, gravel, etc.)
-				.add(DamageTypes.FALLING_ANVIL)        // Falling anvils
-				.add(DamageTypes.FALLING_STALACTITE)   // Falling stalactites
-				.add(DamageTypes.STALAGMITE)          // Running into stalagmites
-				.add(DamageTypes.FLY_INTO_WALL)       // Flying into walls with elytra
-				.add(DamageTypes.CRAMMING)            // Entity cramming
-				.add(DamageTypes.FALL)                // Fall damage
-				.add(DamageTypes.CACTUS)              // Cactus damage
-				.add(DamageTypes.SWEET_BERRY_BUSH)    // Sweet berry bush damage
-				.add(DamageTypes.FREEZE)              // Freezing damage
-				.add(DamageTypes.STARVE)              // Starvation damage
-				.add(DamageTypes.DROWN)               // Drowning damage
-				.add(DamageTypes.DRY_OUT)             // Fish/squid drying out
-				.add(DamageTypes.GENERIC_KILL)        // Generic kill command
-				.add(DamageTypes.STING);              // Bee stings
-
-		// ==========================================
 		// MELEE DAMAGE - Close combat attacks
 		// ==========================================
 		getOrCreateTagBuilder(ModTags.DamageTags.MELEE_DAMAGE)
 				.add(DamageTypes.MOB_ATTACK)          // Melee from mobs
 				.add(DamageTypes.PLAYER_ATTACK)       // Melee from players
-				.addTag(ModTags.DamageTags.DOUBLE_ATTACK)
+				.addOptionalTag(ModTags.DamageTags.DOUBLE_ATTACK)
 				.add(DamageTypes.STING);              // Bee stings (close range)
 
 		// ==========================================
 		// MAGIC DAMAGE - Magical/supernatural forces
 		// ==========================================
 		getOrCreateTagBuilder(ModTags.DamageTags.MAGIC_DAMAGE)
-				.add(DamageTypes.MAGIC)               // Direct magic damage
-				.add(DamageTypes.INDIRECT_MAGIC)      // Indirect magic (potions, etc.)
 				.add(DamageTypes.WITHER_SKULL)        // Wither skull projectiles
 				.add(DamageTypes.DRAGON_BREATH)       // Ender dragon breath
-				.add(DamageTypes.SONIC_BOOM)          // Warden's sonic boom
-				.add(DamageTypes.THORNS)              // Thorns enchantment
 				.add(DamageTypes.WITHER)              // Wither effect
 				.addOptionalTag(DamageTypeTags.WITCH_RESISTANT_TO); // Vanilla magic tag
 
@@ -67,26 +41,12 @@ public class ModDamageTypeTagProvider extends FabricTagProvider<DamageType> {
 		// FIRE DAMAGE - Heat and flame based
 		// ==========================================
 		getOrCreateTagBuilder(ModTags.DamageTags.FIRE_DAMAGE)
-				.add(DamageTypes.IN_FIRE)             // Standing in fire
-				.add(DamageTypes.LAVA)                // Lava damage
-				.add(DamageTypes.ON_FIRE)             // Being on fire
-				.add(DamageTypes.CAMPFIRE)            // Campfire damage
-				.add(DamageTypes.HOT_FLOOR)           // Magma block damage
-				.add(DamageTypes.FIREBALL)            // Ghast/blaze fireballs
-				.add(DamageTypes.UNATTRIBUTED_FIREBALL) // Fireballs without source
 				.addOptionalTag(DamageTypeTags.IS_FIRE); // Vanilla fire tag
 
 		// ==========================================
 		// PROJECTILE DAMAGE - Ranged attacks
 		// ==========================================
 		getOrCreateTagBuilder(ModTags.DamageTags.PROJECTILE_DAMAGE)
-				.add(DamageTypes.ARROW)               // Bow/crossbow arrows
-				.add(DamageTypes.TRIDENT)             // Thrown tridents
-				.add(DamageTypes.MOB_PROJECTILE)      // Projectiles from mobs
-				.add(DamageTypes.FIREBALL)            // Ghast/blaze fireballs
-				.add(DamageTypes.UNATTRIBUTED_FIREBALL) // Fireballs without source
-				.add(DamageTypes.WITHER_SKULL)        // Wither skull projectiles
-				.add(DamageTypes.THROWN)              // Thrown items (snowballs, etc.)
 				.addOptionalTag(DamageTypeTags.IS_PROJECTILE); // Vanilla projectile tag
 
 	}
