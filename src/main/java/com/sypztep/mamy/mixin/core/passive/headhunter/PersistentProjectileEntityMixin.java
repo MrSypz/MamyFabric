02f1ddbe450e90abb193c249523f993b1a9fde97
chain <chainot@hotmail.co.th>
2025-08-12 00:21:01 +0700

    Change to projectile

--- src/main/java/com/sypztep/mamy/mixin/core/passive/headhunter/PersistentProjectileEntityMixin.java
+++ src/main/java/com/sypztep/mamy/mixin/core/passive/headhunter/PersistentProjectileEntityMixin.java
@@ -9,24 +9,22 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.GhastEntity;
 import net.minecraft.entity.passive.SquidEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ProjectileEntity.class)
-public abstract class ProjectileEntityMixin extends Entity {
-    @Shadow
-    @Nullable
-    public abstract Entity getOwner();
+@Mixin(PersistentProjectileEntity.class)
+public abstract class PersistentProjectileEntityMixin extends ProjectileEntity {
 
-    public ProjectileEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
+    public PersistentProjectileEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+        super(entityType, world);
     }
 
     @Inject(method = {"onEntityHit"}, at = {@At("HEAD")})
@@ -46,10 +44,8 @@ public abstract class ProjectileEntityMixin extends Entity {
         double eyeY = hit.getEyeY();
 
         if (y >= eyeY - radius && y <= eyeY + radius) {
-            ModEntityComponents.HEADSHOT.maybeGet(livingTarget).ifPresent(component -> {
-                component.setHeadShot(true);
-                shooter.playSound(ModSoundEvents.ENTITY_GENERIC_HEADSHOT,1.5f,1);
-            });
+            this.playSound(ModSoundEvents.ENTITY_GENERIC_HEADSHOT, 10.0F, 1.0F / (random.nextFloat() * 0.4F + 0.8F));
+            ModEntityComponents.HEADSHOT.maybeGet(livingTarget).ifPresent(component -> component.setHeadShot(true));
         }
     }
 }
