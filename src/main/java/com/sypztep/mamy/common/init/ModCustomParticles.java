de183af5679c13e008d0f72ab1e1c21409954497
chain <chainot@hotmail.co.th>
2025-08-12 01:58:03 +0700

    Headshot particle

--- src/main/java/com/sypztep/mamy/common/init/ModCustomParticles.java
+++ src/main/java/com/sypztep/mamy/common/init/ModCustomParticles.java
@@ -16,6 +16,7 @@ public interface ModCustomParticles {
     TextParticleProvider MISSING = createParticle(Text.translatable("mamy.text.missing"), new Color(1f, 1f, 1f), -0.045f, -0.085F, () -> ModConfig.missingIndicator);
     TextParticleProvider MISSING_MONSTER = createParticle(Text.translatable("mamy.text.missing"), new Color(255, 28, 28), -0.045F, -0.085F, () -> ModConfig.missingIndicator);
     TextParticleProvider BACKATTACK = createParticle(Text.translatable("mamy.text.back"), new Color(1f, 1f, 1f), -0.035f, 0.3f, () -> ModConfig.damageCritIndicator);
+    TextParticleProvider HEADSHOT = createParticle(Text.translatable("mamy.text.headshot"), new Color(ModConfig.critDamageColor), -0.045f, 0.8f, () -> ModConfig.damageCritIndicator);
 
     static void init() {
         System.out.println("ModCustomParticles initialized - registered " + PARTICLES.size() + " particles");
