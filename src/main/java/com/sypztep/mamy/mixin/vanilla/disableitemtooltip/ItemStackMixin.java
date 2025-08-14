0755d4fac867e19009807d9de066561692584eab
chain <chainot@hotmail.co.th>
2025-08-15 04:28:10 +0700

    FIX: armor attribute are not write

--- src/main/java/com/sypztep/mamy/mixin/vanilla/disableitemtooltip/ItemStackMixin.java
+++ src/main/java/com/sypztep/mamy/mixin/vanilla/disableitemtooltip/ItemStackMixin.java
@@ -2,6 +2,8 @@ package com.sypztep.mamy.mixin.vanilla.disableitemtooltip;
 
 import com.sypztep.mamy.client.event.tooltip.CustomArmorTooltip;
 import com.sypztep.mamy.client.event.tooltip.CustomSwordTooltip;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
@@ -24,11 +26,16 @@ public abstract class ItemStackMixin {
     @Inject(method = "appendAttributeModifiersTooltip", at = @At(value = "HEAD"), cancellable = true)
     private void replaceAttributeTooltip(Consumer<Text> textConsumer, @Nullable PlayerEntity player, CallbackInfo ci) {
         ItemStack itemStack = (ItemStack) (Object) this;
-        if (itemStack.getItem() instanceof ArmorItem)
-            CustomArmorTooltip.appendCustomArmorTooltip(itemStack, textConsumer, player);
-         else
-            CustomSwordTooltip.appendCustomAttributeTooltip(itemStack, textConsumer, player);
+
+        AttributeModifiersComponent attributeModifiersComponent = itemStack.getOrDefault(DataComponentTypes.ATTRIBUTE_MODIFIERS, AttributeModifiersComponent.DEFAULT);
+        if (!attributeModifiersComponent.showInTooltip()) return; // Let vanilla handle it
 
         ci.cancel();
+
+        if (itemStack.getItem() instanceof ArmorItem) {
+            CustomArmorTooltip.appendCustomArmorTooltip(itemStack, textConsumer, player);
+        } else {
+            CustomSwordTooltip.appendCustomAttributeTooltip(itemStack, textConsumer, player);
+        }
     }
 }
