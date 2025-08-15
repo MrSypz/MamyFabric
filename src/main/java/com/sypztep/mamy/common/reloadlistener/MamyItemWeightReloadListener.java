4953a80da668ec3342731eec6d49b9c200e3e1d9
chain <chainot@hotmail.co.th>
2025-08-15 22:19:21 +0700

    Just get weight

--- src/main/java/com/sypztep/mamy/common/reloadlistener/MamyItemWeightReloadListener.java
+++ src/main/java/com/sypztep/mamy/common/reloadlistener/MamyItemWeightReloadListener.java
@@ -1,13 +1,9 @@
 package com.sypztep.mamy.common.reloadlistener;
 
 import com.sypztep.mamy.Mamy;
-import com.sypztep.mamy.common.component.ItemWeight;
 import com.sypztep.mamy.common.data.ItemWeightEntry;
-import com.sypztep.mamy.common.init.ModDataComponents;
-import com.sypztep.mamy.common.util.NumberUtil;
 import com.sypztep.mamy.common.util.ReloadHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
@@ -28,24 +24,21 @@ public class MamyItemWeightReloadListener implements SimpleSynchronousResourceRe
         ReloadHelper.processJsonResources(
                 manager,
                 "itemweight",
-                object -> object.has("value"), // validator
+                object -> object.has("weight"),
                 filePath -> Identifier.of(filePath.substring(filePath.indexOf("/") + 1, filePath.length() - 5).replace("/", ":")),
                 Registries.ITEM::get,
                 Registries.ITEM.get(Registries.ITEM.getDefaultId()),
                 (item, json) -> {
-                    float value = json.get("value").getAsFloat();
-                    NumberUtil.WeightUnit unit = json.has("unit") ? NumberUtil.WeightUnit.valueOf(json.get("unit").getAsString().toUpperCase()) : NumberUtil.WeightUnit.GRAM;
-
-                    ItemWeight weight = new ItemWeight(value, unit);
-                    ItemWeightEntry entry = new ItemWeightEntry(weight.toGrams()); // store in grams internally
+                    float weight = json.get("weight").getAsFloat();
+                    ItemWeightEntry entry = new ItemWeightEntry(weight);
                     ItemWeightEntry.ITEM_DATA_MAP.put(item, entry);
 
-                    ItemStack dummy = new ItemStack(item);
-                    dummy.set(ModDataComponents.ITEM_WEIGHT_COMPONENT_TYPE, weight);
-
-                    Mamy.LOGGER.debug("Loaded item weight for '{}': {} {}", Registries.ITEM.getId(item), value, unit);
+                    Mamy.LOGGER.debug("Loaded custom weight for '{}': {}",
+                            Registries.ITEM.getId(item), weight);
                 },
                 "item"
         );
+
+        Mamy.LOGGER.info("Loaded {} custom item weights", ItemWeightEntry.ITEM_DATA_MAP.size());
     }
 }
