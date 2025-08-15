e6785f6468d4a1538ea94718c95abea17b9cc8a0
chain <chainot@hotmail.co.th>
2025-08-16 05:25:57 +0700

    missing tag

--- src/main/java/com/sypztep/mamy/common/init/ModTags.java
+++ src/main/java/com/sypztep/mamy/common/init/ModTags.java
@@ -8,17 +8,18 @@ import net.minecraft.registry.tag.TagKey;
 
 public final class ModTags {
     public static class DamageTags {
+        // Combat damage types
         public static final TagKey<DamageType> MELEE_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("melee_damage"));
-        public static final TagKey<DamageType> MAGIC_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("magic_damage"));
-        public static final TagKey<DamageType> FIRE_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("fire_damage"));
         public static final TagKey<DamageType> PROJECTILE_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("projectile_damage"));
+        public static final TagKey<DamageType> MAGIC_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("magic_damage"));
 
-        public static final TagKey<DamageType> DOUBLE_ATTACK = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("double_attack"));
-
-        public static final TagKey<DamageType> HOLY_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("holy_damage"));
-        public static final TagKey<DamageType> WIND_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("wind_damage"));
-        public static final TagKey<DamageType> WATER_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("water_damage"));
+        // Elemental damage types
+        public static final TagKey<DamageType> FIRE_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("fire_damage"));
         public static final TagKey<DamageType> ELECTRIC_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("electric_damage"));
+        public static final TagKey<DamageType> WATER_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("water_damage"));
+        public static final TagKey<DamageType> WIND_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("wind_damage"));
+        public static final TagKey<DamageType> HOLY_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("holy_damage"));
+        public static final TagKey<DamageType> COLD_DAMAGE = TagKey.of(RegistryKeys.DAMAGE_TYPE, Mamy.id("cold_damage"));
     }
     public static class EntityTypes {
         public static final TagKey<EntityType<?>> CANNOT_HEADSHOT = TagKey.of(RegistryKeys.ENTITY_TYPE, Mamy.id("cannot_headshot"));
