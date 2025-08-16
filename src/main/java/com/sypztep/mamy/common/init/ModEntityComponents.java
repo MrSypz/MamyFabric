5d8958dbc9acbef4117919054691b98cd03a5ffd
chain <chainot@hotmail.co.th>
2025-08-17 05:13:04 +0700

    remove deadcode

--- src/main/java/com/sypztep/mamy/common/init/ModEntityComponents.java
+++ src/main/java/com/sypztep/mamy/common/init/ModEntityComponents.java
@@ -2,8 +2,8 @@ package com.sypztep.mamy.common.init;
 
 import com.sypztep.mamy.Mamy;
 import com.sypztep.mamy.common.component.living.*;
+import com.sypztep.mamy.common.component.living.ability.HeadShotEntityComponent;
 import com.sypztep.mamy.common.component.living.ability.PhantomWalkerComponent;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -18,9 +18,8 @@ public final class ModEntityComponents implements EntityComponentInitializer {
     public static final ComponentKey<DamageTrackerComponent> DAMAGETRACKER = ComponentRegistry.getOrCreate(Mamy.id("dmgtracker"), DamageTrackerComponent.class);
     public static final ComponentKey<PlayerClassComponent> PLAYERCLASS = ComponentRegistry.getOrCreate(Mamy.id("playerclass"), PlayerClassComponent.class);
     public static final ComponentKey<PlayerStanceComponent> PLAYERSTANCE = ComponentRegistry.getOrCreate(Mamy.id("playerstance"), PlayerStanceComponent.class);
-    public static final ComponentKey<HeadShotEntityComponent> HEADSHOT = ComponentRegistry.getOrCreate(Mamy.id("headshot"), HeadShotEntityComponent.class);
     public static final ComponentKey<PlayerWeightComponent> PLAYERWEIGHT = ComponentRegistry.getOrCreate(Mamy.id("playerweight"), PlayerWeightComponent.class);
-
+    public static final ComponentKey<HeadShotEntityComponent> HEADSHOT = ComponentRegistry.getOrCreate(Mamy.id("headshot"), HeadShotEntityComponent.class);
     public static final ComponentKey<PhantomWalkerComponent> PHANTOMWALKER = ComponentRegistry.getOrCreate(Mamy.id("phantomwalker"), PhantomWalkerComponent.class);
 
 
@@ -29,10 +28,10 @@ public final class ModEntityComponents implements EntityComponentInitializer {
         registry.beginRegistration(LivingEntity.class, LIVINGLEVEL).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(LivingLevelComponent::new);
         registry.beginRegistration(PlayerEntity.class, PLAYERCLASS).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(PlayerClassComponent::new);
         registry.registerFor(MobEntity.class, DAMAGETRACKER, entity -> new DamageTrackerComponent());
-        registry.registerFor(LivingEntity.class, HEADSHOT, HeadShotEntityComponent::new);
         registry.registerForPlayers(PLAYERSTANCE, PlayerStanceComponent::new, RespawnCopyStrategy.ALWAYS_COPY);
         registry.registerForPlayers(PLAYERWEIGHT, PlayerWeightComponent::new, RespawnCopyStrategy.ALWAYS_COPY);
 
+        registry.registerFor(LivingEntity.class, HEADSHOT, HeadShotEntityComponent::new);
         registry.registerFor(PlayerEntity.class, PHANTOMWALKER, PhantomWalkerComponent::new);
     }
 }
