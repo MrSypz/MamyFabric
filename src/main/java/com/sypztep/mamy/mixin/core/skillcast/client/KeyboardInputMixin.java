6f3098d80791d7524b23ba45c9995972b70d87c7
chain <chainot@hotmail.co.th>
2025-09-06 05:49:16 +0700

    Lock Movement

--- src/main/java/com/sypztep/mamy/mixin/core/skillcast/client/KeyboardInputMixin.java
+++ src/main/java/com/sypztep/mamy/mixin/core/skillcast/client/KeyboardInputMixin.java
@@ -1,9 +1,17 @@
 package com.sypztep.mamy.mixin.core.skillcast.client;
 
+import com.sypztep.mamy.client.event.animation.CrowdControlAnimationManager;
 import com.sypztep.mamy.common.system.skill.SkillCastingManager;
+import com.sypztep.mamy.common.util.MovementLock;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.input.KeyboardInput;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,7 +21,12 @@ public class KeyboardInputMixin extends Input {
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void lockMovementDuringCast(boolean slowDown, float slowDownFactor, CallbackInfo ci) {
-        if (SkillCastingManager.getInstance().shouldLockMovement()) {
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if (player == null) return;
+
+        boolean shouldLock = SkillCastingManager.getInstance().shouldLockMovement() || shouldLockMovement(player);
+
+        if (shouldLock) {
             pressingForward = false;
             pressingBack = false;
             pressingLeft = false;
@@ -21,5 +34,27 @@ public class KeyboardInputMixin extends Input {
             movementForward = 0;
             movementSideways = 0;
         }
+    }
+    @Unique
+    private boolean hasMovementLockingEffect(LivingEntity entity) {
+        for (StatusEffectInstance effectInstance : entity.getStatusEffects()) {
+            StatusEffect effect = effectInstance.getEffectType().value();
+            if (effect instanceof MovementLock movementLock && movementLock.shouldLockMovement()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Unique
+    private boolean shouldLockMovement(LivingEntity entity) {
+        boolean hasLockingEffect = hasMovementLockingEffect(entity);
+
+        if (entity.getWorld().isClient()) {
+            boolean animationPlaying = CrowdControlAnimationManager.isCrowdControlAnimationPlaying();
+            return hasLockingEffect || animationPlaying;
+        }
+
+        return hasLockingEffect;
     }
 }
