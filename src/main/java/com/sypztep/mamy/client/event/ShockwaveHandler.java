801585f6281d56a68c666b48c895c13f350c3c66
mrsypz <chainot@hotmail.co.th>
2025-09-09 13:34:17 +0700

    comment clearify

--- src/main/java/com/sypztep/mamy/client/event/ShockwaveHandler.java
+++ src/main/java/com/sypztep/mamy/client/event/ShockwaveHandler.java
@@ -1,4 +1,4 @@
-package com.sypztep.mamy.client.event;accessoraccessor
+package com.sypztep.mamy.client.event;
 
 import com.sypztep.mamy.client.screen.CameraShakeManager;
 import com.sypztep.mamy.common.init.ModSoundEvents;
