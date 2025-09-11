65ca5dbbf19e0b5bd6076004e999b3b464c1c0ed
chain <chainot@hotmail.co.th>
2025-09-11 12:59:15 +0700

    Animation clean up

--- src/main/java/com/sypztep/mamy/common/event/player/PlayerDisconnectCleanupEvent.java
+++ src/main/java/com/sypztep/mamy/common/event/player/PlayerDisconnectCleanupEvent.java
@@ -1,7 +1,9 @@
 package com.sypztep.mamy.common.event.player;
 
+import com.sypztep.mamy.client.event.animation.NetworkAnimationManager;
 import com.sypztep.mamy.common.system.skill.SkillManager;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 
@@ -12,8 +14,9 @@ public final class PlayerDisconnectCleanupEvent implements ServerPlayConnectionE
 
     @Override
     public void onPlayDisconnect(ServerPlayNetworkHandler handler, MinecraftServer server) {
-        String playerId = handler.getPlayer().getUuidAsString();
+        PlayerEntity player = handler.player;
 
-        SkillManager.cleanupPlayer(playerId);
+        SkillManager.cleanupPlayer(player.getUuidAsString());
+        NetworkAnimationManager.cleanupPlayer(player.getId());
     }
 }
